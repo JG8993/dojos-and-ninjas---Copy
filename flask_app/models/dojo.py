323af0c5c34950c3768d6472e0bc778b3dba28dd
JG8993 <105567446+JG8993@users.noreply.github.com>
2022-12-12 17:12:46 -0800

    Update dojo.py

--- flask_app/models/dojo.py
+++ flask_app/models/dojo.py
@@ -30,7 +30,7 @@ class Dojo:
         results = connectToMySQL('dojos_and_ninjas_schema_jg').query_db(query,data)
         print(results)
         dojo = cls(results[0])
-        for n in results:
+        for row in results:
             n = {
                 'id': row['ninjas.id'],
                 'first_name': row['first_name'],
